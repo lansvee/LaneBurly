26d5630c066bd47885e8bc1a7d0270b4d9fde9e2
[lansvee] <lanie.verzosa2@gmail.com>
2025-02-27 21:51:04 +0800

    add fix

--- src/App.js
+++
@@ -1,8 +0,0 @@
-// src/App.js
-import React from 'react';
-import Home from './pages/Home';
-
-export default function App() {
-  // render <Home />
-  return <Home />;
-}
--- src/context/UserContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// Create the context
-const UserContext = React.createContext();
-
-// Provides the context to the components
-export const UserProvider = UserContext.Provider;
-
-export default UserContext;
